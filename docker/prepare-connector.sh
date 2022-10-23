aaa668f5631b05e35d483b70d737f776d7e36c6f
hardworkar <arsmoroz@ya.ru>
2022-10-23 15:41:54 +0700

    Added command to copy connector for Windows users

--- docker/prepare-connector.sh
+++ docker/prepare-connector.sh
@@ -2,3 +2,5 @@
 export JAR_PATH="../Scim2Connector/scim-2-connector/target/scim-2-connector-1.0-SNAPSHOT.jar"
 
 cp $JAR_PATH ./midpoint/pio/icf-connectors/
+
+# Windows users: copy "..\Scim2Connector\scim-2-connector\target\scim-2-connector-1.0-SNAPSHOT.jar" ".\midpoint\pio\icf-connectors"

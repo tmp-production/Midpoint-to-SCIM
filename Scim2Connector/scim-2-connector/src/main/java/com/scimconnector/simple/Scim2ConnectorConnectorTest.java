136169dde90a1c1ba4ea8fb83bbfdb79deb9f7ee
hardworkar <arsmoroz@ya.ru>
2022-11-27 19:28:42 +0700

    Implemented Midpoint-side part of E2E CreateUser test.

--- Scim2Connector/scim-2-connector/src/main/java/com/scimconnector/simple/Scim2ConnectorConnectorTest.java
+++ Scim2Connector/scim-2-connector/src/main/java/com/scimconnector/simple/Scim2ConnectorConnectorTest.java
@@ -1,8 +1,19 @@
 package com.scimconnector.simple;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.identityconnectors.framework.common.objects.ObjectClassInfo;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
 
 class Scim2ConnectorConnectorTest {
     private final static String HOST_NAME = "http://0.0.0.0:8081";
@@ -65,5 +76,75 @@ class Scim2ConnectorConnectorTest {
 
         Assertions.assertNull(connector.getConnection());
         Assertions.assertNull(connector.getConnection());
+    }
+
+    List<String> getMidpointUsernames() {
+        WebClient webClient = WebClient.create("http://localhost:8080/");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ArrayList<String> usernames = new ArrayList<>();
+        String users = webClient.get()
+                .uri("midpoint/ws/rest/users")
+                .header("Authorization", "Basic YWRtaW5pc3RyYXRvcjo1ZWNyM3Q=")
+                .header("Accept", "application/json")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(users);
+
+        JsonNode root = null;
+        try {
+            root = objectMapper.readTree(users);
+
+            JsonNode userArray = root.get("object").get("object");
+            assert userArray.isArray();
+            Iterator<JsonNode> it = userArray.elements();
+            while (it.hasNext()) {
+                JsonNode user = it.next();
+                assert user.isObject();
+                usernames.add(user.get("name").asText());
+            }
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return usernames;
+    }
+
+    /**
+     * all necessary docker instances must be running to pass tests listed below
+     */
+    @org.junit.jupiter.api.Test
+    void e2eCreateTest() {
+        WebClient webClient = WebClient.create("http://localhost:8080/");
+
+        String newUserName = UUID.randomUUID().toString();
+        String newDescription = UUID.randomUUID().toString();
+        String newFullName = UUID.randomUUID().toString();
+        String newGivenName = UUID.randomUUID().toString();
+
+        String body = String.format("<user>\n" +
+                "    <name>%s</name>\n" +
+                "    <description>%s</description>\n" +
+                "    <fullName>%s</fullName>\n" +
+                "    <givenName>%s</givenName>\n" +
+                "    <familyName>AutomaticallyGenerated</familyName>\n" +
+                "</user>\n", newUserName, newDescription, newFullName, newGivenName);
+
+        String requestRes = webClient.post()
+                .uri("midpoint/ws/rest/users")
+                .header("Authorization", "Basic YWRtaW5pc3RyYXRvcjo1ZWNyM3Q=")
+                .header("Content-Type", "application/xml")
+                .body(BodyInserters.fromValue(body))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        //TODO wait a little here,
+        // then check validity through SCIM-REST (GET /users), parse list to see that
+        // our new user propagated to SCIM successfully
+
+        System.out.println(requestRes);
     }
 }
